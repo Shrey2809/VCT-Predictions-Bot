f51289782e0d26fcc3cc4252a7643e6e7f8f9d92
SHREY MANDOL <mandol@ualberta.ca>
2024-01-07 22:05:57 +0000

    new table added

--- tables_stuff.py
+++ tables_stuff.py
@@ -1,74 +1,38 @@
 import sqlite3
 
 # Connect to a new or existing SQLite database
-conn = sqlite3.connect('VCT_2024_963095570124251136.db')
-# conn = sqlite3.connect('VCT_2024_1042862967072501860.db')
+# conn = sqlite3.connect('VCT_2024_963095570124251136.db')
+conn = sqlite3.connect('VCT_2024_1042862967072501860.db')
 
 # Create a cursor object to interact with the database
 cursor = conn.cursor()
 
 # SQL commands to create tables with auto-incremented primary key and adjusted user_id field
 create_table_queries = [
-    '''
-    Create TABLE DS_2024_FILES_PROCESSED (
-        id        INTEGER PRIMARY KEY AUTOINCREMENT,
-        file_name VARCHAR(50),
-        created_date_time   DATETIME DEFAULT CURRENT_TIMESTAMP
-    );
-    ''',
-    '''
-    CREATE TABLE DS_2024_KICKOFF (
-        id        INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_name VARCHAR(50),
-        user_id   BIGINT,
-        americas  INT DEFAULT 0,
-        emea      INT DEFAULT 0,
-        china     INT DEFAULT 0,
-        pacific   INT DEFAULT 0
-    );
-    ''',
-    '''
-    CREATE TABLE DS_2024_IL1 (
-        id        INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_name VARCHAR(50),
-        user_id   BIGINT,
-        americas  INT DEFAULT 0,
-        emea      INT DEFAULT 0,
-        china     INT DEFAULT 0,
-        pacific   INT DEFAULT 0
-    );
-    ''',
-    '''
-    CREATE TABLE DS_2024_IL2 (
-        id        INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_name VARCHAR(50),
-        user_id   BIGINT DEFAULT 0,
-        americas  INT DEFAULT 0,
-        emea      INT DEFAULT 0,
-        china     INT DEFAULT 0,
-        pacific   INT DEFAULT 0
-    );
-    ''',
-    '''
-    CREATE TABLE DS_2024_MASTERS (
-        id                INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_name         VARCHAR(50),
-        user_id           BIGINT,
-        shanghai_groups   INT DEFAULT 0,
-        shanghai_playoffs INT DEFAULT 0,
-        madrid_groups     INT DEFAULT 0,
-        madrid_playoffs   INT DEFAULT 0
-    );
-    ''',
-    '''
-    CREATE TABLE DS_2024_CHAMPIONS (
-        id              INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_name       VARCHAR(50),
-        user_id         BIGINT,
-        korea_groups    INT DEFAULT 0,
-        korea_playoffs  INT DEFAULT 0
-    );
-    '''
+    """
+    CREATE TABLE DS_VCT_2024 (
+        id                        INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_name                 VARCHAR(50),
+        user_id                   BIGINT,
+        kickoff_americas          INT DEFAULT 0,
+        kickoff_emea              INT DEFAULT 0,
+        kickoff_china             INT DEFAULT 0,
+        kickoff_pacific           INT DEFAULT 0,
+        il1_americas              INT DEFAULT 0,
+        il1_emea                  INT DEFAULT 0,
+        il1_china                 INT DEFAULT 0,
+        il1_pacific               INT DEFAULT 0,
+        il2_americas              INT DEFAULT 0,
+        il2_emea                  INT DEFAULT 0,
+        il2_china                 INT DEFAULT 0,
+        il2_pacific               INT DEFAULT 0,
+        masters_shanghai_groups   INT DEFAULT 0,
+        masters_shanghai_playoffs INT DEFAULT 0,
+        masters_madrid_groups     INT DEFAULT 0,
+        masters_madrid_playoffs   INT DEFAULT 0,
+        champions_korea_groups    INT DEFAULT 0,
+        champions_korea_playoffs  INT DEFAULT 0
+    );"""
 ]
 
 
